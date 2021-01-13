04ad4796316f4d098e3c70e855d45d7740c213fa
physcrowley <crowlda@ecolecatholique.ca>
2021-01-13 12:07:06 -0500

    initial commit

---
+++ exemple2/src/main/java/edu/ics4u/exemple2/HelloWorld.java
@@ -0,0 +1,10 @@
+package edu.ics4u.exemple2;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+        System.exit(0);
+    }
+
+}
